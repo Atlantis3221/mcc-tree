2895e90367ec0dc59c2dbb1747b24dffbd0705af
Atlantis3221 <stepatugarev98@gmail.com>
2023-09-12 16:09:46 +0700

    fix: remove children items, add parent item

--- src/shared/layout/Layout.tsx
+++ src/shared/layout/Layout.tsx
@@ -1,56 +1,89 @@
 import Header from '../../widgets/Header/Header'
 import Footer from '../../widgets/Footer/Footer'
 import Tree from '../../widgets/Tree/Tree'
+import { AppButton } from '../ui/AppButton/AppButton'
+import { useState } from 'react'
+
+export interface ITree {
+  key: string
+  label: string
+  children?: ITree[]
+}
 
 export default function Layout() {
 
-    const treeData = [
-        {
-          key: "0",
-          label: "Documents",
-          children: [
-            {
-              key: "0-0",
-              label: "Document 1-1",
-              children: [
-                {
-                  key: "0-1-1",
-                  label: "Document-0-1.doc",
-                },
-                {
-                  key: "0-1-2",
-                  label: "Document-0-2.doc",
-                },
-              ],
-            },
-          ],
-        },
+  const treeData: ITree[] = [
+    {
+      key: "0",
+      label: "Documents",
+      children: [
         {
           key: "1",
-          label: "Desktop",
+          label: "Document 0-0",
           children: [
             {
-              key: "1-0",
-              label: "document1.doc",
+              key: "2",
+              label: "Document-0-0-1.doc",
             },
             {
-              key: "0-0",
-              label: "documennt-2.doc",
+              key: "3",
+              label: "Document-0-0-2.doc",
             },
           ],
         },
+      ],
+    },
+    {
+      key: "4",
+      label: "Desktop",
+      children: [
         {
-          key: "2",
-          label: "Downloads",
-          children: [],
+          key: "5",
+          label: "document-1.doc",
         },
-      ];
+        {
+          key: "6",
+          label: "document-2.doc",
+        },
+      ],
+    },
+    {
+      key: "7",
+      label: "Downloads",
+      children: [],
+    },
+  ];
 
-    return (
-        <>
-            <Header />
-            <Tree treeData={treeData} />
-            <Footer />
-        </>
-    )
+  const [mutableTree, setMutableTree] = useState([...treeData])
+
+  //по ключу удаляет родительский элемент
+  function deleteItem(key: string) {
+    setMutableTree(mutableTree.filter((item) => item.key !== key))
+  }
+
+  //возвращает дерево к первоначальному виду
+  const resetTree = () => {
+    setMutableTree(treeData)
+  }
+
+  //добавляет объект на верхний уровень дерева, в перспективе добавил бы форму для редактирования глубины дочерних элементов и уникальный ключ
+  const addItem = () => {
+    setMutableTree([...mutableTree, {
+      key: "8",
+      label: "2nd Downloads",
+      children: [],
+    }])
+  }
+
+  return (
+    <>
+      <Header />
+      <div>
+        <AppButton onClick={resetTree} text='reset' />
+        <AppButton onClick={addItem} text='add item' />
+      </div>
+      <Tree mutableTree={mutableTree} deleteItem={deleteItem} />
+      <Footer />
+    </>
+  )
 }
--- src/widgets/Tree/Tree.tsx
+++ src/widgets/Tree/Tree.tsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react";
+import { AppButton } from "../../shared/ui/AppButton/AppButton";
 import TreeItem from "../../shared/ui/TreeItem/TreeItem";
+import './style.css'
 
-export default function Tree({ treeData }) {
+export default function Tree({ mutableTree, deleteItem }: any) {
+
     return (
-        <ul>
-            {treeData.map((node:any) => (
-                <TreeItem node={node} key={node.key} />
-            ))}
-        </ul>
+        <>
+            <ul style={{ padding: '32px' }}>
+                {mutableTree.map((node: any) => (
+                    <div className="item-wrapper">
+                    <TreeItem node={node} key={node.key} deleteItem={deleteItem} />
+                    </div>
+                ))}
+            </ul>
+        </>
     );
 }
